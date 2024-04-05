fd802a548422b89ce0037596a223d819eda51320
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-05 11:37:11 -0300

    feita a estrutura do Sobre mim, faltando apenas o css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Hero.css'
-import profile_img from '../../assets/profile_img.svg'
 
 const Hero = () => {
     return (
@@ -10,7 +9,7 @@ const Hero = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus excepturi explicabo minima reiciendis mollitia</p>
             <div className="hero-action">
                 <div className="hero-connect">Conecte-se comigo</div>
-                <div className="hero-resume">Sobre mim</div>
+                <div className="hero-resume">Meu Currículo</div>
             </div>
         </div>
     )
--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
 import "../Sobre/Sobre.css"
-const Sobre = () => {
-  return (
-    <div className='sobre'>
+import theme_pattern from '../../assets/theme_pattern.svg'
 
-    </div>
-  )
+
+const Sobre = () => {
+    return (
+        <div className='sobre'>
+            <div className="sobre-title">
+                <h1>Sobre mim</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="sobre-section">
+                <div className="sobre-left">
+                <img id='eu-sobre' src='src\assets\img_curriculo.jpg' alt="" />
+                </div>
+                <div className="sobre-right">
+                    <div className="sobre-para">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis saepe ab, quam recusandae nostrum voluptatum possimus ducimus iste eos voluptate distinctio id veritatis aliquam libero explicabo iusto nemo aspernatur beatae.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, quam ad mollitia placeat vero sunt quasi dignissimos nam blanditiis saepe quae numquam ut nisi? Esse ullam aut veniam eos quae?</p>
+                    </div>
+                    <div className="sobre-skills">
+                        <div className="sobre-skill"><p>HTML & CSS</p><hr style={{width: "50%"}} /></div>
+                        <div className="sobre-skill"><p>React JS & Node.js</p><hr style={{width: "70%"}} /></div>
+                        <div className="sobre-skill"><p>Javascript & TestCafe</p><hr style={{width: "60%"}} /></div>
+                        <div className="sobre-skill"><p>Java Springboot & JUnit</p><hr style={{width: "50%"}} /></div>
+                    </div>
+                </div>
+            </div>
+            <div className="sobre-achievements">
+                <div className="sobre-achievement">
+                    <h1>2</h1>
+                    <p>ANOS DE EXPERIÊNCIA</p>
+                </div>
+                <hr />
+                <div className="sobre-achievement">
+                    <h1>Xs</h1>
+                    <p>PROJETOS FEITOS</p>
+                </div>
+                <hr />
+                <div className="sobre-achievement">
+                    <h1>1</h1>
+                    <p>PROTÓTIPO NO FIGMA</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Sobre
