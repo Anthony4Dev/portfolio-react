93a8da98920e272f827698e29994c3fd04e61f1d
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-04 21:19:12 -0300

    ajuste de itens e gradiente, foto temporária

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,9 +5,9 @@ import profile_img from '../../assets/profile_img.svg'
 const Hero = () => {
     return (
         <div className='hero'>
-            <img src={profile_img} alt="" />
-            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus excepturi explicabo minima reiciendis mollitia, dolor modi officia rem aperiam illo quibusdam, quos magnam asperiores totam ullam fugiat impedit eius maiores.</p>
+            <img id='eu' src='src\assets\img_curriculo.jpg' alt="" />
+            <h1><span> Me chamo Islan Anthony</span> sou desenvolvedor web.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus excepturi explicabo minima reiciendis mollitia</p>
             <div className="hero-action">
                 <div className="hero-connect">Conecte-se comigo</div>
                 <div className="hero-resume">Sobre mim</div>
