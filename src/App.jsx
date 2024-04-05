a34eaf61d3aa6e2eef6833d889a9d51e2e011742
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-05 11:06:47 -0300

    começo da criação da aba Sobre mim

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Sobre from './Components/Sobre/Sobre'
 const App = () => {
   return (
     <div>
       
     <Navbar/>
     <Hero/>
-
+  <Sobre/>
     </div>
   )
 }
---
+++ src/Components/Sobre/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Sobre/Sobre.css"
+const Sobre = () => {
+  return (
+    <div className='sobre'>
+
+    </div>
+  )
+}
+
+export default Sobre
