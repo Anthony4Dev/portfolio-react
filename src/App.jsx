9d7c1edc535684f759404f73a5498f1ab2253f86
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-06 16:48:18 -0300

    criando os elementos de contato

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Hero from './Components/Hero/Hero'
 import Sobre from './Components/Sobre/Sobre'
 import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
+
 const App = () => {
   return (
     <div>
-      
-    <Navbar/>
-    <Hero/>
-  <Sobre/>
-  <Services/>
-  <MyWork/>
+      <Navbar />
+      <Hero />
+      <Sobre />
+      <Services />
+      <MyWork />
+      <Contact />
     </div>
   )
 }
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../Contact/Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className="contact-title">
+                <h1>Entre em contato</h1>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Vamos conversar!</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente numquam id earum hic laboriosam nostrum alias. Error tenetur a delectus, fugiat eligendi, blanditiis aperiam dignissimos provident, corrupti sint sit totam.</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src={mail_icon} alt="" /> <p>anthony.islan2005@gmail.com</p>
+                        </div>
+
+                        <div className="contact-detail">
+                            <img src={call_icon} alt="" /> <p>+55 (81) 991395701</p>
+                        </div>
+
+                        <div className="contact-detail">
+                            <img src={location_icon} alt="" /> <p>Caruaru, PE, Brasil</p>
+                        </div>
+                    </div>
+                </div>
+                <form className="contact-right">
+                    <label htmlFor="">Islan Anthony</label>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
