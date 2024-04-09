e70c30cf7796fe4517aa7a0d61ce1e2915888f4f
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-09 14:45:12 -0300

    adicionado o deploy do consumo de api de pokedex

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import '../Services/Services.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import Services_Data from '../../assets/services_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import React from 'react';
+import '../Services/Services.css';
+import theme_pattern from '../../assets/theme_pattern.svg';
+import Services_Data from '../../assets/services_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
+
 const Services = () => {
+    const handleServiceClick = (serviceNo) => {
+        const serviceLinks = {
+            '01': 'link-do-servico-01',
+            '02': 'link-do-servico-02',
+            '03': 'link-do-servico-03',
+            '04': 'https://anthony4dev.github.io/pokedex/' 
+        };
+
+        if (serviceNo in serviceLinks) {
+            // Redirecionar para o link correspondente
+            window.location.href = serviceLinks[serviceNo];
+        }
+    };
+
     return (
         <div id='serviços' className='services'>
             <div className="services-title">
@@ -12,19 +27,21 @@ const Services = () => {
             </div>
             <div className="services-container">
                 {Services_Data.map((service,index) => {
-                    return <div key={index} className="services-format">
-                        <h3>{service.s_no}</h3>
-                        <h2>{service.s_name}</h2>
-                        <p>{service.s_desc}</p>
-                        <div className="services-readmore">
-                            <p>Saiba mais</p>
-                            <img src={arrow_icon} alt="" />
+                    return (
+                        <div key={index} className="services-format" onClick={() => handleServiceClick(service.s_no)}>
+                            <h3>{service.s_no}</h3>
+                            <h2>{service.s_name}</h2>
+                            <p>{service.s_desc}</p>
+                            <div className="services-readmore">
+                                <p>Saiba mais</p>
+                                <img src={arrow_icon} alt="" />
+                            </div>
                         </div>
-                    </div>
+                    );
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Services
+export default Services;
