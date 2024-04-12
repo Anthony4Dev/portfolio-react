594b22215dec1c154c9c28256b17bc68d11e2814
Anthony4dev <anthony.islan2005@gmail.com>
2024-04-12 15:09:21 -0300

    teste de correção de deploy

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,7 +12,6 @@ const Contact = () => {
     const [messageError, setMessageError] = useState('');
 
     const validateEmail = (email) => {
-        // Expressão regular para validar o formato do e-mail
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
     };
